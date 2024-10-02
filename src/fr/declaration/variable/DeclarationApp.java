ec98203986623b44661bbbc1890e1e70ede94286
hdesain-diginamic <hugodesain@gmail.com>
2024-10-02 14:01:44 +0200

    TP 2 terminé

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,18 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String args[]) {
+        byte a = 12;
+        short b = -15325;
+        int c = 1250326;
+        long d = 10000L;
+        float e = 1.8F;
+        double f = 1.5D;
+        char g = 'a';
+        boolean i = true;
+        String h = "Bonjour";
+        String randomString = "Voici le résultat d’un calcul : \n1+5=6";
+
+        System.out.println(randomString);
+    }
+}
